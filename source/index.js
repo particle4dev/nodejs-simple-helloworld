8d456e664187576cdbef9879aded01ae50895527
hoang <particle4dev@gmail.com>
2016-10-30 18:56:04 +0700

    [src] added appId

--- source/index.js
+++ source/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+var appId = Math.floor((Math.random() * 1000) + 1);
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! appId=' + appId);
 });
 
 app.listen(3000, function () {
