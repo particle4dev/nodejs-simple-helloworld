d7c6ad2c812946810b3ed0b80d6fe3c8f88bd31d
hoang <particle4dev@gmail.com>
2017-02-13 01:49:01 +0700

    update circle.yml : VERSION

--- 0circleci-deployment/deploy.sh
+++ 0circleci-deployment/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+VERSION=$1
 configure_aws_cli(){
   aws --version
   aws configure set default.region ap-northeast-1
@@ -7,8 +7,8 @@ configure_aws_cli(){
 }
 
 push_ecr_image(){
-  eval $(aws ecr get-login --region us-east-1)
-  docker push 073861455897.dkr.ecr.ap-northeast-1.amazonaws.com/nodejs-simple-helloworld:$CIRCLE_SHA1
+  eval $(aws ecr get-login --region ap-northeast-1)
+  docker push 073861455897.dkr.ecr.ap-northeast-1.amazonaws.com/nodejs-simple-helloworld:$VERSION
 }
 
 configure_aws_cli
